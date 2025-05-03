598b005fc95a74ce21b5075acc65ee5875481598
Adam W <adam@igmax.com>
2025-05-02 17:54:17 -0700

    Start ble client proj

---
+++ main/ble/lispif_ble_client_extensions.h
@@ -0,0 +1,26 @@
+/*  
+	Copyright 2025  
+  
+	This file is part of the VESC firmware.  
+  
+	The VESC firmware is free software: you can redistribute it and/or modify  
+	it under the terms of the GNU General Public License as published by  
+	the Free Software Foundation, either version 3 of the License, or  
+	(at your option) any later version.  
+  
+	The VESC firmware is distributed in the hope that it will be useful,  
+	but WITHOUT ANY WARRANTY; without even the implied warranty of  
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+	GNU General Public License for more details.  
+  
+	You should have received a copy of the GNU General Public License  
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+*/  
+  
+#ifndef MAIN_BLE_LISPIF_BLE_CLIENT_EXTENSIONS_H_  
+#define MAIN_BLE_LISPIF_BLE_CLIENT_EXTENSIONS_H_  
+  
+// Load BLE client extensions into the LispBM environment  
+void lispif_load_ble_client_extensions(void);  
+  
+#endif /* MAIN_BLE_LISPIF_BLE_CLIENT_EXTENSIONS_H_ */
